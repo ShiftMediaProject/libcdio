c9487cad26a60a30481f1a80fcf6bf48e2c1dd47
rocky <rocky>
2003-04-06 06:43:32 +0000

    Go with Linux definitions rather than invent our own. More changes will no doubt follow.

--- lib/sector.h
+++ lib/sector.h
@@ -1,5 +1,5 @@
 /*
-    $Id: sector.h,v 1.3 2003/04/03 12:16:27 rocky Exp $
+    $Id: sector.h,v 1.4 2003/04/06 06:43:32 rocky Exp $
 
     Copyright (C) 2000 Herbert Valerio Riedel <hvr@gnu.org>
     Copyright (C) 2003 Rocky Bernstein <rocky@panix.com>
@@ -30,11 +30,6 @@
 #define CDIO_PREGAP_SECTORS 150
 #define CDIO_POSTGAP_SECTORS 150
 
-/*! 
-  Constant for ending or "leadout" track.
-*/
-#define CDIO_LEADOUT_TRACK  0xaa
-
 /*
  * A CD-ROM physical sector size is 2048, 2052, 2056, 2324, 2332, 2336, 
  * 2340, or 2352 bytes long.  
@@ -43,7 +38,7 @@
  *
  * format   sector type               user data size (bytes)
  * -----------------------------------------------------------------------------
- *   1     (Red Book)    CD-DA          2352    (CDDA_SECTOR_SIZE)
+ *   1     (Red Book)    CD-DA          2352    (CD-FRAMESIZE_RAW)
  *   2     (Yellow Book) Mode1 Form1    2048    (M1F1_SECTOR_SIZE)
  *   3     (Yellow Book) Mode1 Form2    2336    (M2RAW_SECTOR_SIZE)
  *   4     (Green Book)  Mode2 Form1    2048    (M2F1_SECTOR_SIZE)
@@ -69,8 +64,96 @@
  *
  */
 
-#define CD_RAW_SECTOR_SIZE  2352
-#define CDDA_SECTOR_SIZE    CD_RAW_SECTOR_SIZE
+/* 
+   Some generally useful CD-ROM information -- mostly based on the above.
+   This is from linux.h - not to slight other OS's. This was the first
+   place I came across such useful stuff.
+*/
+#ifndef CD_MINS
+#define CD_MINS              74 /* max. minutes per CD, not really a limit */
+#endif
+#ifndef CD_SECS
+#define CD_SECS              60 /* seconds per minute */
+#endif
+#ifndef CD_FRAMES
+#define CD_FRAMES            75 /* frames per second */
+#endif
+#ifndef CD_SYNC_SIZE
+#define CD_SYNC_SIZE         12 /* 12 sync bytes per raw data frame */
+#endif
+#ifndef CD_MSF_OFFSET
+#define CD_MSF_OFFSET       150 /* MSF numbering offset of first frame */
+#endif
+#ifndef CD_CHUNK_SIZE
+#define CD_CHUNK_SIZE        24 /* lowest-level "data bytes piece" */
+#endif
+#ifndef CD_NUM_OF_CHUNKS
+#define CD_NUM_OF_CHUNKS     98 /* chunks per frame */
+#endif
+#ifndef CD_FRAMESIZE_SUB
+#define CD_FRAMESIZE_SUB     96 /* subchannel data "frame" size */
+#endif
+#ifndef CD_HEAD_SIZE
+#define CD_HEAD_SIZE          4 /* header (address) bytes per raw data frame */
+#endif
+#ifndef CD_SUBHEAD_SIZE
+#define CD_SUBHEAD_SIZE       8 /* subheader bytes per raw XA data frame */
+#endif
+#ifndef CD_EDC_SIZE
+#define CD_EDC_SIZE           4 /* bytes EDC per most raw data frame types */
+#endif
+#ifndef CD_ZERO_SIZE
+#define CD_ZERO_SIZE          8 /* bytes zero per yellow book mode 1 frame */
+#endif
+#ifndef CD_ECC_SIZE
+#define CD_ECC_SIZE         276 /* bytes ECC per most raw data frame types */
+#endif
+#ifndef CD_FRAMESIZE
+#define CD_FRAMESIZE       2048 /* bytes per frame, "cooked" mode */
+#endif
+#ifndef CD_FRAMESIZE_RAW
+#define CD_FRAMESIZE_RAW   2352 /* bytes per frame, "raw" mode */
+#endif
+#ifndef CD_FRAMESIZE_RAWER
+#define CD_FRAMESIZE_RAWER 2646 /* The maximum possible returned bytes */ 
+#endif
+/* most drives don't deliver everything: */
+#ifndef CD_FRAMESIZE_RAW1
+#define CD_FRAMESIZE_RAW1 (CD_FRAMESIZE_RAW-CD_SYNC_SIZE) /*2340*/
+#endif
+#ifndef CD_FRAMESIZE_RAW0
+#define CD_FRAMESIZE_RAW0 (CD_FRAMESIZE_RAW-CD_SYNC_SIZE-CD_HEAD_SIZE) /*2336*/
+#endif
+
+#ifndef CD_XA_HEAD
+#define CD_XA_HEAD        (CD_HEAD_SIZE+CD_SUBHEAD_SIZE) /* "before data" part of raw XA frame */
+#endif
+#ifndef CD_XA_TAIL
+#define CD_XA_TAIL        (CD_EDC_SIZE+CD_ECC_SIZE) /* "after data" part of raw XA frame */
+#endif
+#ifndef CD_XA_SYNC_HEAD
+#define CD_XA_SYNC_HEAD   (CD_SYNC_SIZE+CD_XA_HEAD) /* sync bytes + header of XA frame */
+#endif
+
+/* CD-ROM address types (cdrom_tocentry.cdte_format) */
+#ifndef CDROM_LBA
+#define	CDROM_LBA 0x01 /* "logical block": first frame is #0 */
+#endif
+#ifndef CDROM_MSF
+#define	CDROM_MSF 0x02 /* "minute-second-frame": binary, not bcd here! */
+#endif
+
+/* bit to tell whether track is data or audio (cdrom_tocentry.cdte_ctrl) */
+#ifndef CDROM_DATA_TRACK
+#define	CDROM_DATA_TRACK	0x04
+#endif
+
+/* The leadout track is always 0xAA, regardless of # of tracks on disc */
+#ifndef CDROM_LEADOUT
+#define	CDROM_LEADOUT		0xAA
+#endif
+
+
 #define M1F1_SECTOR_SIZE    2048
 #define M2F1_SECTOR_SIZE    2048
 #define FORM1_DATA_SIZE     2048  /* Mode 1 or Mode 2 user data */
