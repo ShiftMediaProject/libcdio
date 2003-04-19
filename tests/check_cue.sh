1121be4e45c0b1b46b4d249d98b314a7b5253da9
rocky <rocky>
2003-04-19 19:12:06 +0000

    Break most of the larger regression tests into a separate package.

--- tests/check_cue.sh
+++ tests/check_cue.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#$Id: check_cue.sh,v 1.4 2003/04/14 08:51:14 rocky Exp $
+#$Id: check_cue.sh,v 1.5 2003/04/19 19:12:06 rocky Exp $
 
 if test -z $srcdir ; then
   srcdir=`pwd`
@@ -9,32 +9,44 @@ fi
 
 BASE=`basename $0 .sh`
 
-fname=fsf
-test_cdinfo "--cue-file ${srcdir}/${fname}.cue --no-cddb" \
-  ${fname}.dump ${srcdir}/${fname}.right
-RC=$?
-check_result $RC 'cdinfo CUE test 1'
-
 fname=fsf-tompox
+testnum=1
 test_cdinfo "--cue-file ${srcdir}/${fname}.cue" \
   ${fname}.dump ${srcdir}/${fname}.right
 RC=$?
-check_result $RC 'cdinfo CUE test 2'
-
-fname=svcd_ogt_test_ntsc
-if test -f ${srcdir}/${fname}.cue ; then
-  test_cdinfo "--cue-file ${srcdir}/${fname}.cue" \
-    ${fname}.dump ${srcdir}/${fname}.right
-  RC=$?
-  check_result $RC 'cdinfo CUE test 3'
-fi
+check_result $RC "cdinfo CUE test $testnum"
 
 fname=vcd_demo
+testnum=2
 if test -f ${srcdir}/${fname}.cue ; then
   test_cdinfo "-c ${srcdir}/vcd_demo.cue" \
       ${fname}.dump ${srcdir}/${fname}.right
   RC=$?
-  check_result $RC 'cdinfo CUE test 4'
+  check_result $RC "cdinfo CUE test $testnum"
+else 
+  echo "Don't see CUE file ${srcdir}/${fname}.cue. Test $testum skipped."
+fi
+
+fname=svcd_ogt_test_ntsc
+testnum=3
+if test -f ${srcdir}/${fname}.bin ; then
+  test_cdinfo "--cue-file ${srcdir}/${fname}.cue" \
+    ${fname}.dump ${srcdir}/${fname}.right
+  RC=$?
+  check_result $RC "cdinfo CUE test $testnum"
+else 
+  echo "Don't see CUE file ${srcdir}/${fname}.bin. Test $testnum skipped."
+fi
+
+fname=fsf
+testnum=4
+if test -f  ${srcdir}/${fname}.bin ; then
+  test_cdinfo "--cue-file ${srcdir}/${fname}.cue --no-cddb" \
+    ${fname}.dump ${srcdir}/${fname}.right
+  RC=$?
+  check_result $RC "cdinfo CUE test $testnum"
+else 
+  echo "Don't see CUE file ${srcdir}/${fname}.bin. Test $testum skipped."
 fi
 
 exit $RC
--- tests/check_nrg.sh
+++ tests/check_nrg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#$Id: check_nrg.sh,v 1.1 2003/03/24 19:01:10 rocky Exp $
+#$Id: check_nrg.sh,v 1.2 2003/04/19 19:12:06 rocky Exp $
 
 if test -z $srcdir ; then
   srcdir=`pwd`
@@ -8,13 +8,19 @@ fi
 . ${srcdir}/check_common_fn
 
 BASE=`basename $0 .sh`
+nrg_file=${srcdir}/monvoisin.nrg
 
-test_cdinfo "--nrg-file ${srcdir}/monvoisin.nrg" \
-  monvoisin.dump ${srcdir}/monvoisin.right
-RC=$?
-check_result $RC 'cdinfo NRG test 1'
-
-exit $RC
+if test -f  $nrg_file ; then
+  test_cdinfo "--nrg-file $nrg_file" \
+    monvoisin.dump ${srcdir}/monvoisin.right
+  RC=$?
+  check_result $RC 'cdinfo NRG test 1'
+  
+  exit $RC
+else 
+  echo "Don't see NRG file ${nrg_file}. Test skipped."
+  exit $SKIP_TEST_EXITCODE
+fi
 
 #;;; Local Variables: ***
 #;;; mode:shell-script ***
