d21266807aae35c0fb9dc8cd5ff78b8c1374a430
rocky <rocky>
2004-11-13 23:36:22 +0000

    Add explicit NULL for get_hwinfo.

--- lib/_cdio_sunos.c
+++ lib/_cdio_sunos.c
@@ -1,5 +1,5 @@
 /*
-    $Id: _cdio_sunos.c,v 1.77 2004/11/13 23:33:57 rocky Exp $
+    $Id: _cdio_sunos.c,v 1.78 2004/11/13 23:36:22 rocky Exp $
 
     Copyright (C) 2001 Herbert Valerio Riedel <hvr@gnu.org>
     Copyright (C) 2002, 2003, 2004 Rocky Bernstein <rocky@panix.com>
@@ -38,7 +38,7 @@
 
 #ifdef HAVE_SOLARIS_CDROM
 
-static const char _rcsid[] = "$Id: _cdio_sunos.c,v 1.77 2004/11/13 23:33:57 rocky Exp $";
+static const char _rcsid[] = "$Id: _cdio_sunos.c,v 1.78 2004/11/13 23:36:22 rocky Exp $";
 
 #ifdef HAVE_GLOB_H
 #include <glob.h>
@@ -844,6 +844,7 @@ cdio_open_am_solaris (const char *psz_orig_source, const char *access_mode)
   _funcs.get_discmode       = get_discmode_solaris;
   _funcs.get_drive_cap      = scsi_mmc_get_drive_cap_generic;
   _funcs.get_first_track_num= get_first_track_num_generic;
+  _funcs.get_hwinfo         = NULL;
   _funcs.get_mcn            = scsi_mmc_get_mcn_generic,
   _funcs.get_num_tracks     = get_num_tracks_generic;
   _funcs.get_track_format   = get_track_format_solaris;
