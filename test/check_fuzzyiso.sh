ccf435d4d8e909ace42716a37e5b45bc3295bd3b
rocky <rocky>
2005-03-03 01:04:27 +0000

    Change to make "make distcheck" (build outside of src directory) work.

--- test/check_fuzzyiso.sh
+++ test/check_fuzzyiso.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#$Id: check_fuzzyiso.sh,v 1.3 2005/03/02 12:49:28 rocky Exp $
+#$Id: check_fuzzyiso.sh,v 1.4 2005/03/03 01:04:27 rocky Exp $
 
 if test -z $srcdir ; then
   srcdir=`pwd`
@@ -9,15 +9,20 @@ if test -z $top_srcdir ; then
   top_srcdir=`pwd`/..
 fi
 
-check_program="$top_srcdir/example/isofuzzy"
+if test -z $top_builddir ; then
+  top_builddir=`pwd`/..
+fi
+
+check_program="$top_builddir/example/isofuzzy"
 
 if test ! -x $check_program ; then
   exit 77
 fi
 
-for file in *.bin *.iso *.nrg ; do 
+cd $srcdir; src_dir=`pwd`
+for file in $src_dir/*.bin $src_dir/*.iso $src_dir/*.nrg ; do 
   case "$file" in
-  cdda.bin | cdda-mcn.nrg | svcdgs.nrg )
+  $src_dir/cdda.bin | $src_dir/cdda-mcn.nrg | $src_dir/svcdgs.nrg )
     good_rc=1
     ;;
   *) 
@@ -26,6 +31,8 @@ for file in *.bin *.iso *.nrg ; do
   esac
   $check_program $file
   if test $? -ne $good_rc ; then 
+    echo "$0: failed running:"
+    echo "	$check_program $file"
     exit 1
   fi
 done
