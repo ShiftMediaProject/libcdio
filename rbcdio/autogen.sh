3bf7aa81c3e1998fe55378209c144c55d629e128
rocky <rocky>
2006-11-13 05:19:13 +0000

    Remove files checked into the wrong place

--- rbcdio/autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#$Id: autogen.sh,v 1.1 2006/11/13 05:12:41 rocky Exp $
-aclocal -I .
-automake --add-missing
-autoconf
--- rbcdio/lib/extconf.rb
+++
@@ -1,4 +0,0 @@
-require 'mkmf'
-dir_config('rubycdio')
-$libs = append_library($libs, "cdio")
-create_makefile('rubycdio')
