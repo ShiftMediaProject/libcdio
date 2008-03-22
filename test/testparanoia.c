06c83dd486733c1fd948f247ba1a85344eaa0116
karl <karl>
2008-03-22 18:08:25 +0000

    gplv3

--- test/testparanoia.c
+++ test/testparanoia.c
@@ -1,21 +1,22 @@
 /*
- * Copyright (C) 2005, 2006 Rocky Bernstein <rocky@panix.com>
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2, or (at your option)
- *   any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
+  $Id: testparanoia.c,v 1.13 2008/03/22 18:08:25 karl Exp $
+
+  Copyright (C) 2005, 2006, 2008 Rocky Bernstein <rocky@gnu.org>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /* Simple program to show using libcdio's version of cdparanoia. */
 
 #ifdef HAVE_CONFIG_H
