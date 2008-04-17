0e61f3de1ab3f8c662af37eeb55edbc2eb1b8ba1
karl <karl>
2008-04-17 17:39:47 +0000

    gplv3+

--- lib/paranoia/gap.h
+++ lib/paranoia/gap.h
@@ -1,22 +1,21 @@
 /*
-  $Id: gap.h,v 1.1 2004/12/18 17:29:32 rocky Exp $
+  $Id: gap.h,v 1.2 2008/04/17 17:39:48 karl Exp $
 
-  Copyright (C) 2004 Rocky Bernstein <rocky@panix.com>
+  Copyright (C) 2004, 2008 Rocky Bernstein <rocky@gnu.org>
   Copyright (C) 1998 Monty xiphmont@mit.edu
   
-  This program is free software; you can redistribute it and/or modify
+  This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
+  the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef _GAP_H_
