2328a4872c54e0ada4edc7028b8499cf19c8b516
karl <karl>
2008-04-20 13:44:31 +0000

    gplv3+

--- lib/cdio++/devices.cpp
+++ lib/cdio++/devices.cpp
@@ -1,21 +1,20 @@
 /* -*- C++ -*-
-    $Id: devices.cpp,v 1.2 2007/03/10 23:57:58 rocky Exp $
+  $Id: devices.cpp,v 1.3 2008/04/20 13:44:31 karl Exp $
 
-    Copyright (C) 2006 Rocky Bernstein <rocky@panix.com>
+  Copyright (C) 2006, 2008 Rocky Bernstein <rocky@gnu.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "config.h"
 #ifdef HAVE_SYS_TYPES_H
