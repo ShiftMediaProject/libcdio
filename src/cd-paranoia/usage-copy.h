cc08ce617f81eecb1112ec9b50b01f272da613d9
rocky <rocky@sanchez.(none)>
2009-05-16 23:01:39 -0400

    README.develop: add help2man. Rest - cosmetic changes

--- src/cd-paranoia/usage-copy.h
+++ src/cd-paranoia/usage-copy.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 1999, 2005, 2008 Rocky Bernstein
+  Copyright (C) 1999, 2005, 2008, 2009 Rocky Bernstein
    
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
