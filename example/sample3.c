8002d97db8fcb7ece855af4bdab0fb6f30b13e59
R. Bernstein <rocky@gnu.org>
2011-10-19 22:01:37 -0400

    Remove CVS $Id$ line which is no longer automatically updated. ios9660.hpp under FS because of Solaris macro conflict as suggested by Thomas Schmitt. cd-drive.c: add some casts to remove gcc warnings src/cd-paranoia/Makefile.am: remove a GNU make idiom. README.develop: note needing GNU make or remake

--- example/sample3.c
+++ example/sample3.c
@@ -1,7 +1,5 @@
 /*
-  $Id: sample3.c,v 1.10 2008/03/24 15:30:56 karl Exp $
-
-  Copyright (C) 2003, 2005, 2008 Rocky Bernstein <rocky@gnu.org>
+  Copyright (C) 2003, 2005, 2008, 2011 Rocky Bernstein <rocky@gnu.org>
   
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
