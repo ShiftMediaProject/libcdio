be5e4108ce9396da61089344a015fd542184f08d
R. Bernstein <rocky@gnu.org>
2012-04-14 22:17:51 -0400

    Update copyright

--- lib/driver/_cdio_generic.c
+++ lib/driver/_cdio_generic.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2011
+  Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2011, 2012
   Rocky Bernstein <rocky@gnu.org>
 
   This program is free software: you can redistribute it and/or modify
