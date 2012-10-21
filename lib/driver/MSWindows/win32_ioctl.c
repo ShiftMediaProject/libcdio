51860ff8d529497f94e7678cba9b5684990b153c
R. Bernstein <rocky@gnu.org>
2012-10-21 15:35:36 -0400

    Small stuff: Indent win32_ioctl.c defines. ignore all ~'s

--- lib/driver/MSWindows/win32_ioctl.c
+++ lib/driver/MSWindows/win32_ioctl.c
@@ -31,15 +31,15 @@
 # include "NtScsi.h"
 # include "undocumented.h"
 #else
-#if defined (__MINGW64__)
-# define _NTSRB_ /* Bad things happen if srb.h gets included */
-# include <windows.h>
-# include <ntddcdrm.h>
-# include <ntddscsi.h>
-#else
-# include <ddk/ntddcdrm.h>
-# include <ddk/ntddscsi.h>
-#endif
+# if defined (__MINGW64__)
+#  define _NTSRB_ /* Bad things happen if srb.h gets included */
+#  include <windows.h>
+#  include <ntddcdrm.h>
+#  include <ntddscsi.h>
+# else
+#  include "ntddcdrm.h"
+#  include "ntddscsi.h"
+# endif
 # include <ddk/scsi.h>
 #endif
 
