0125534c32ca01c25d40ca3430584bf36b24347c
R. Bernstein <rocky@gnu.org>
2013-09-20 12:10:42 -0400

    Add a custom log handler.

--- example/logging.c
+++ example/logging.c
@@ -15,9 +15,15 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/* Simple program to show how to set log verbosity. And write a custom
-   log handler.
+/* Simple program to show how to set log verbosity and how to write a
+   custom log handler.
+
+   Invoke with an argument level, e.g.
+      logging debug
+   or with a number like:
+      logging 1 # same as debug
 */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #define __CDIO_CONFIG_H__ 1
@@ -46,12 +52,38 @@
 #include <cdio/cd_types.h>
 #include <cdio/logging.h>
 
+
+/* Here is an example of a custom log handler. */
+static void
+custom_log_handler (cdio_log_level_t level, const char *message)
+{
+  switch(level) {
+  case CDIO_LOG_DEBUG:
+    printf("-- custom debug message: %s\n", message);
+    return;
+  case CDIO_LOG_INFO:
+    printf("-- custom info message: %s\n", message);
+    return;
+  case CDIO_LOG_WARN:
+    printf("-- custom warning message: %s\n", message);
+    return;
+  case CDIO_LOG_ERROR:
+    printf("-- custom error message: %s\n", message);
+    return;
+  case CDIO_LOG_ASSERT:
+    printf("-- custom fatal error message: %s\n", message);
+    return;
+  default:
+    printf("custom level %d message: %s\n", level, message);
+  }
+}
+
 static long
-parse_loglevel(const char *ps_loglevel) {
+parse_loglevel_arg(const char *ps_loglevel) {
     long loglevel;
     if (strncmp(ps_loglevel, "debug", strlen("debug")) == 0) {
 	loglevel = CDIO_LOG_DEBUG;
-    } else if (strncmp(ps_loglevel, "info", strlen("info"))) {
+    } else if (strncmp(ps_loglevel, "info", strlen("info")) == 0) {
 	loglevel = CDIO_LOG_INFO;
     } else if (strncmp(ps_loglevel, "warn", strlen("warn")) == 0) {
 	loglevel = CDIO_LOG_WARN;
@@ -81,27 +113,10 @@ parse_loglevel(const char *ps_loglevel) {
     return loglevel;
 }
 
-
-int
-main(int argc, const char *argv[])
+static void
+print_drives()
 {
     char **ppsz_cd_drives=NULL, **c;
-    /* Set the log level to the least verbosity. */
-    cdio_loglevel_default = CDIO_LOG_WARN;
-
-    /* Parse command arguemnts */
-    if (argc > 2) {
-	printf("Usage:\n%s <verbosity-level>\n"
-	       "Shows cd drives with given verbosity level",
-	       argv[0]);
-	exit(EXIT_FAILURE);
-    }
-
-    if (argc == 2) {
-	/* Set cdio's verbosity level. */
-        cdio_loglevel_default = parse_loglevel(argv[1]);
-    }
-
     /* Print out a list of CD-drives with the above set log level. */
     ppsz_cd_drives = cdio_get_devices(DRIVER_DEVICE);
     if (NULL != ppsz_cd_drives)
@@ -109,5 +124,34 @@ main(int argc, const char *argv[])
 	    printf("-- Drive %s\n", *c);
 	}
     cdio_free_device_list(ppsz_cd_drives);
+}
+
+int
+main(int argc, const char *argv[])
+{
+    /* Set the log level to the least verbosity. */
+    cdio_loglevel_default = CDIO_LOG_WARN;
+
+    /* Parse command arguemnts */
+    if (argc > 2) {
+	printf("Usage:\n%s <verbosity-level>\n"
+	       "Shows drives.\n"
+	       "A verbosity level is a name like 'debug', 'info', 'warn,'\n"
+	       "or a number greater than 0.\n",
+	       argv[0]);
+	exit(EXIT_FAILURE);
+    }
+
+    if (argc == 2) {
+	/* Set cdio's verbosity level. */
+        cdio_loglevel_default = parse_loglevel_arg(argv[1]);
+    }
+
+    print_drives();
+
+    /* Do the same thing again but with a custom log handler. */
+    cdio_log_set_handler (custom_log_handler);
+
+    print_drives();
     return 0;
 }
