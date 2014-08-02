fb2c7fab8d44b5aceb82e3d3b6a04a2d3912ecc2
Matthew Oliver <protogonoi@gmail.com>
2014-08-02 11:54:49 +1000

    Add VS project files.

---
+++ SMC/cdio_version.h
@@ -0,0 +1,9 @@
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libcdio 0.92"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.92"
+
+/* Version number of package */
+#define VERSION "0.92"
