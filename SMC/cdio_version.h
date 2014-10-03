6017a6ded865215c3da5763d9804db10fb093907
Matthew Oliver <protogonoi@gmail.com>
2014-10-03 18:11:53 +1000

    Update VS projects for recent merge.

--- SMC/cdio_version.h
+++ SMC/cdio_version.h
@@ -1,9 +1,8 @@
-
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libcdio 0.92"
+#define PACKAGE_STRING "libcdio 0.93"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.92"
+#define PACKAGE_VERSION "0.93"
 
 /* Version number of package */
-#define VERSION "0.92"
+#define VERSION "0.93"
