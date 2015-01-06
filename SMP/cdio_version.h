9beed8064ca48aa7a714b9ac227a0aabbf57c3a2
Matt Oliver <protogonoi@gmail.com>
2015-01-06 17:17:04 +1100

    project: Cleanup config header.

--- SMP/cdio_version.h
+++
@@ -1,8 +0,0 @@
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libcdio 0.93"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "0.93"
-
-/* Version number of package */
-#define VERSION "0.93"
--- SMP/config.h
+++ SMP/config.h
@@ -1,8 +1,3 @@
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
-
-/* Define if building universal (internal helper macro) */
-/* #undef AC_APPLE_UNIVERSAL_BUILD */
 
 /* Define 1 if you are compiling using cygwin */
 /* #undef CYGWIN */
@@ -310,13 +305,6 @@
    script can define this before including any of libcdio's headers. */
 #define LIBCDIO_CONFIG_H 1
 
-/* Full path to libcdio top_sourcedir. */
-#define LIBCDIO_SOURCE_PATH "/d/Code_Files/ShiftMediaCenter/dependencies/source/libcdio"
-
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
-#define LT_OBJDIR ".libs/"
-
 /* Define 1 if you are compiling using MinGW */
 /* #undef MINGW32 */
 
@@ -327,85 +315,5 @@
    In cygwin it is a function too */
 /* #undef NEED_TIMEZONEVAR */
 
-/* Name of package */
-#define PACKAGE "libcdio"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "https://savannah.gnu.org/bugs/?group=libcdio"
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "libcdio"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libcdio"
-
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
-
-/* Enable extensions on AIX 3, Interix.  */
-#ifndef _ALL_SOURCE
-# define _ALL_SOURCE 1
-#endif
-/* Enable GNU extensions on systems that have them.  */
-#ifndef _GNU_SOURCE
-# define _GNU_SOURCE 1
-#endif
-/* Enable threading extensions on Solaris.  */
-#ifndef _POSIX_PTHREAD_SEMANTICS
-# define _POSIX_PTHREAD_SEMANTICS 1
-#endif
-/* Enable extensions on HP NonStop.  */
-#ifndef _TANDEM_SOURCE
-# define _TANDEM_SOURCE 1
-#endif
-/* Enable general extensions on Solaris.  */
-#ifndef __EXTENSIONS__
-# define __EXTENSIONS__ 1
-#endif
-
-#include "cdio_version.h"
-
-/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-   significant byte first (like Motorola and SPARC, unlike Intel). */
-#if defined AC_APPLE_UNIVERSAL_BUILD
-# if defined __BIG_ENDIAN__
-#  define WORDS_BIGENDIAN 1
-# endif
-#else
-# ifndef WORDS_BIGENDIAN
-/* #  undef WORDS_BIGENDIAN */
-# endif
-#endif
-
-/* Enable large inode numbers on Mac OS X 10.5.  */
-#ifndef _DARWIN_USE_64_BIT_INODE
-# define _DARWIN_USE_64_BIT_INODE 1
-#endif
-
-/* Number of bits in a file offset, on hosts where this is settable. */
-#define _FILE_OFFSET_BITS 64
-
-/* Define for large files, on AIX-style hosts. */
-/* #undef _LARGE_FILES */
-
-/* Define to 1 if on MINIX. */
-/* #undef _MINIX */
-
-/* Define to 2 if the system does not provide POSIX.1 features except with
-   this defined. */
-/* #undef _POSIX_1_SOURCE */
-
-/* Define to 1 if you need to in order for `stat' and other things to work. */
-/* #undef _POSIX_SOURCE */
-
-/* Define to empty if `const' does not conform to ANSI C. */
-/* #undef const */
-
-/* Define to `__inline__' or `__inline' if that's what the C compiler
-   calls it, or to nothing if 'inline' is not supported under any name.  */
-#ifndef __cplusplus
-/* #undef inline */
-#endif
