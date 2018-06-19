9ea591f825ffe6a7d493c2c031d3b9e32a091e01
Edd Barrett <vext01@gmail.com>
2018-06-19 21:20:42 +0100

    NetBSD driver: Improve device enumeration and make it work on OpenBSD.

--- lib/driver/netbsd.c
+++ lib/driver/netbsd.c
@@ -61,6 +61,8 @@
 #define DEFAULT_CDIO_DEVICE "/dev/rcd0c"
 #endif
 
+#define MAX_CD_DEVICES 64
+
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
@@ -78,6 +80,7 @@
 #include <sys/cdio.h>
 #include <sys/scsiio.h>
 #include <sys/sysctl.h>
+#include <sys/disklabel.h>
 
 #define TOTAL_TRACKS (_obj->tochdr.ending_track \
                       - _obj->tochdr.starting_track + 1)
@@ -629,50 +632,24 @@ cdio_get_devices_netbsd (void)
 #ifndef HAVE_NETBSD_CDROM
   return NULL;
 #else
-  char drive[40];
+  char drive[16];
   char **drives = NULL;
-  unsigned int num_drives=0;
-  int mib[2];
-  size_t len;
-  char *p, *pp, *data;
+  unsigned int num_drives = 0;
+  int cdfd;
+  int n;
 
-  mib[0] = CTL_HW;
-  mib[1] = HW_DISKNAMES;
-
-  /* Determine how much space to allocate. */
-  if (sysctl(mib, 2, NULL, &len, NULL, 0) == -1)
-     return NULL;
-
-  if ((data = (char *)malloc(len)) == NULL)
-     return NULL;
-
-  if (sysctl(mib, 2, data, &len, NULL, 0) == -1) {
-     free(data);
-     return NULL;
-  }
-  if (sizeof DEFAULT_CDIO_DEVICE > sizeof drive) {
-     free(data);
-     return NULL;
-  }
-  strcpy(drive,DEFAULT_CDIO_DEVICE);
-  p = data;
-  while ((pp = strchr(p, ' ')) != NULL) {
-     *pp = '\0';
-     if (p[0] == 'c' && p[1] == 'd' && p[2] >= '0' && p[2] <= '9' &&
-         p[3] == '\0') {
-        drive[sizeof DEFAULT_CDIO_DEVICE - 3] = p[2];
-        cdio_add_device_list(&drives, drive, &num_drives);
-     }
-     p = ++pp;
-  }
-  if (p[0] == 'c' && p[1] == 'd' && p[2] >= '0' && p[2] <= '9' &&
-      p[3] == '\0') {
-     drive[sizeof DEFAULT_CDIO_DEVICE - 3] = p[2];
-     cdio_add_device_list(&drives, drive, &num_drives);
+  /* Search for open(2)able /dev/rcd* devices */
+  for (n = 0; n <= MAX_CD_DEVICES; n++) {
+    snprintf(drive, sizeof(drive), "/dev/rcd%d%c", n, 'a' + RAW_PART);
+    if (!cdio_is_device_quiet_generic(drive))
+      continue;
+    if ((cdfd = open(drive, O_RDONLY|O_NONBLOCK, 0)) == -1)
+      continue;
+    close(cdfd);
+    cdio_add_device_list(&drives, drive, &num_drives);
   }
   cdio_add_device_list(&drives, NULL, &num_drives);
-  free(data);
-  return drives;
+  return (drives);
 #endif /* HAVE_NETBSD_CDROM */
 }
 
